e8a48fe746d42f51c4014d7c74a77da7e648363e
park-yina <kln99988@gmail.com>
2024-06-04 12:41:21 +0900

    static 제거하여 conflict오류 해결

--- userprog/process.c
+++ userprog/process.c
@@ -425,7 +425,7 @@ done:
 
 /* Checks whether PHDR describes a valid, loadable segment in
  * FILE and returns true if so, false otherwise. */
-static bool
+bool
 validate_segment (const struct Phdr *phdr, struct file *file) {
 	/* p_offset and p_vaddr must have the same page offset. */
 	if ((phdr->p_offset & PGMASK) != (phdr->p_vaddr & PGMASK))
