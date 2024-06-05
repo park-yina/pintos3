e9122d36b4e61289d34c864a0db844b3938546f5
park-yina <kln99988@gmail.com>
2024-06-05 20:07:13 +0900

    process.h테스트

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -8,18 +8,13 @@ tid_t process_fork (const char *name, struct intr_frame *if_);
 int process_exec (void *f_name);
 int process_wait (tid_t);
 void process_exit (void);
-bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
-		uint32_t read_bytes, uint32_t zero_bytes,
-		bool writable);
-
 void process_activate (struct thread *next);
-struct container{
+void argument_stack (int argc, char **argv, struct intr_frame *if_);
+
+#endif /* userprog/process.h */
+
+struct container {
     struct file *file;
     off_t offset;
     size_t page_read_bytes;
 };
-#define ELF ELF64_hdr
-#define Phdr ELF64_PHDR
-
-bool validate_segment(const struct Phdr *phdr, struct file *file);
-#endif /* userprog/process.h */
