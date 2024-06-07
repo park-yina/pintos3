7b4f0cc656eafe413ea53031ec4a24f89ccfd70b
park-yina <kln99988@gmail.com>
2024-06-07 21:36:19 +0900

    syscall.c수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -34,6 +34,21 @@ void syscall_handler(struct intr_frame *);
 #define STDIN_fileNO 0
 #define STDOUT_fileNO 1
 
+void check_address(void *addr);
+void halt(void);
+void exit(int status);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+int open(const char *file_name);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned size);
+int write(int fd, const void *buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
+int fork(const char *thread_name, struct intr_frame *f);
+int exec(const char *cmd_line);
+int wait(int pid);
 
 static struct file *find_file_by_fd(int fd)
 {
