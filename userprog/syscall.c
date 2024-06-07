8bf280fc412b8c65eaf9aea0d06ea813b2e3a58f
park-yina <kln99988@gmail.com>
2024-06-07 21:16:50 +0900

    들여쓰기 수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -143,13 +143,13 @@ void syscall_handler(struct intr_frame *f)
 	case SYS_CLOSE:
 		close(f->R.rdi);
 		break;
-		case SYS_MMAP:
+	case SYS_MMAP:
 			f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
 			break;
-		case SYS_MUNMAP:
+	case SYS_MUNMAP:
 			munmap(f->R.rdi);
 			break;
-		default:
+	default:
 			exit(-1);
 			break;
 	}
