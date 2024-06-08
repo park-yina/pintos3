2a0976997ae442c4f94c9b888d03d6c8fab18fa5
park-yina <kln99988@gmail.com>
2024-06-08 14:15:00 +0900

    page_less수정

--- vm/vm.c
+++ vm/vm.c
@@ -346,8 +346,8 @@ unsigned page_hash(const struct hash_elem *p_, void *aux UNUSED)
 
 bool page_less(const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED)
 {
-    const struct page *a = hash_entry(a_, struct page, h_elem);
-    const struct page *b = hash_entry(b_, struct page, h_elem);
+    const struct page *a = hash_entry(a_, struct page, hash_elem);
+    const struct page *b = hash_entry(b_, struct page, hash_elem);
 
     return a->va < b->va;
 }
